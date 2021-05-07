25e402c81479089041c65d406aa3285a0631c3c0
Krzysiek <krzys@piekarczyk.eu>
2021-05-07 16:10:29 +0200

    qs

--- src/org/kmp/morpheus/Triangle.java
+++ src/org/kmp/morpheus/Triangle.java
@@ -45,15 +45,15 @@ public class Triangle implements Comparable<Triangle> {
         float c = (float) Math.sqrt(Math.pow(points[0].x - points[1].x, 2) + Math.pow(points[0].y - points[1].y, 2) + Math.pow(points[0].z - points[1].z, 2));
 
 
-        if (a >= b  && a >= c) {
+        if (a >= b && a >= c) {
             centroid.x = (points[1].x + points[2].x) / 2;
             centroid.y = (points[1].y + points[2].y) / 2;
             centroid.z = (points[1].z + points[2].z) / 2;
-        } else if (b >= a  && b >= c) {
+        } else if (b >= a && b >= c) {
             centroid.x = (points[2].x + points[0].x) / 2;
             centroid.y = (points[2].y + points[0].y) / 2;
             centroid.z = (points[2].z + points[0].z) / 2;
-        } else if (c >= b  && c >= a) {
+        } else if (c >= b && c >= a) {
             centroid.x = (points[0].x + points[1].x) / 2;
             centroid.y = (points[0].y + points[1].y) / 2;
             centroid.z = (points[0].z + points[1].z) / 2;
@@ -73,7 +73,21 @@ public class Triangle implements Comparable<Triangle> {
 
     @Override
     public int compareTo(Triangle t) {
-        return Float.compare(t.distance, distance);
+//        return Float.compare(t.distance, distance);
+        PVector vPQ = new PVector(t.points[1].x - t.points[0].x, t.points[1].y - t.points[0].y, t.points[1].z - t.points[0].z);
+        PVector vPR = new PVector(t.points[2].x - t.points[0].x, t.points[2].y - t.points[0].y, t.points[2].z - t.points[0].z);
+
+        PVector perpV = vPQ.cross(vPR);
+        float a = perpV.x;
+        float b = perpV.y;
+        float c = perpV.z;
+        float x0 = vPQ.x;
+        float y0 = vPQ.y;
+        float z0 = vPQ.z;
+
+        // a * (x - x0) + b * (y - y0) + c * (z - z0) = 0
+
+
     }
 
     public List<Edge> edgeList() {
